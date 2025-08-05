509a2729b51f1cc9c4a747d9439a81da2722bf29
Refiloe Mopeloa <72868391+refiloemopeloa@users.noreply.github.com>
2025-08-05 21:37:51 +0200

    Finalized Bandit class and added algortihms

--- Bandit.py
+++ Bandit.py
@@ -12,44 +12,100 @@ class Arm:
         return np.random.normal(loc=self.mean, scale=self.std_dev, size=1)   
 
 class Bandit:
-    def __init__(self, arms, selection_algorithm):
-        self.arms = arms
-        self.selection_algorithm = selection_algorithm
-        self.total_reward = 0.0
-        self.pull_history = []
-
-    def select_arm(self, step=None):
-        if step is not None:
-            return self.selection_algorithm.select_arm(self.arms, step)
-        return self.selection_algorithm.select_arm(self.arms)
-
-    def pull_arm(self, step=None):
-        arm_index = self.select_arm(step)
-        reward = float(self.arms[arm_index].get_reward())  
-        self.total_reward += reward
-        self.pull_history.append((arm_index, reward))
-        return arm_index, reward
-
-    def reset(self):
-        self.total_reward = 0.0
-        self.pull_history.clear()
-
-    def get_total_reward(self):
-        return self.total_reward
-
-    def run(self, rounds, optimal_expected_value=None):
-        rewards = []
-        regret = []
-        cumulative_reward = 0.0
-        cumulative_regret = 0.0
+    arms:list = []
+    count:int = 0
+    total_reward:float = 0
+    pull_count = []
+    arm_id:int = 0
+    
+    def __init__(self, count=10):
+        self.count = count
+        for i in range(self.count):
+            self.arms.append(Arm())
+        self.reset()
         
-        for t in range(1, rounds+1):
-            _, reward = self.pull_arm(step=t)
-            cumulative_reward += reward
-            rewards.append(cumulative_reward / t)
-
-            if optimal_expected_value is not None:
-                cumulative_regret += (optimal_expected_value - reward)
-                regret.append(cumulative_regret)
-                
-        return rewards, regret
+    def reset(self):
+        self.total_reward = 0
+        self.pull_count = np.zeros(self.count)
+        self.estimated_values = np.zeros(self.count)
+        self.rewards_history = []
+        self.actions_history = []
+        
+    def pull_arm(self, arm_id):
+        # obtain the current arms reward
+        reward = self.arms[arm_id].get_reward()
+        
+        # keep track of the number of times this arm is pulled
+        self.pull_count[arm_id] += 1
+        
+        # add the current reward to the total reward
+        self.total_reward += reward
+        
+        # Update Q: Qn+1 = Qn + (Rn - Qn)/n
+        self.estimated_values[arm_id] += (reward - self.estimated_values[arm_id])/self.pull_count[arm_id]
+        
+        # keep track of the reward from the taken action at this time step
+        self.rewards_history.append(reward)
+        
+        # keep track of the action that was taken at each time step
+        self.actions_history.append(arm_id)
+        
+        return reward
+    
+    # this function wll help when we need to calculate optimal action percentage
+    def get_optimal_arm(self):
+        # Return the argmax of the arm that gives the highest mean
+        return np.argmax([arm.mean for arm in self.arms])
+    
+class EpsilonGreedy:
+    epsilon = 0
+    
+    def __init__(self, epsilon=0.1):
+        self.epsilon = epsilon
+        
+    def select_arm(self, bandit:Bandit):
+        # check if arm should explore or exploit
+        if np.random.random() < self.epsilon:
+            # explore
+            return np.random.randint(bandit.count)
+        else:
+            # exploit
+            return np.argmax(bandit.estimated_values)
+        
+class OptimisticGreedy:
+    Q1 = 0
+    initialized = False
+    def __init__(self, Q1=5.0):
+        self.Q1 = Q1
+        
+    def select_arm(self, bandit:Bandit):
+        # check if the optimistic value has been initialized, that is, has the estimated values for all arms been set to Q1
+        if not self.initialized:
+            bandit.estimated_values.fill(self.Q1)
+            self.initialized = True
+            
+        # choose the highest estimate of every subsequent value throughout the computation of the estimate
+        return np.argmax(bandit.estimated_values)
+    
+class UCB:
+    c:float = 0
+    
+    def __init__(self, c:float = 2):
+        self.c = c
+    
+    def select_arm(self, bandit:Bandit):
+        # obtain which "time step" we are at right now
+        total_pulls = np.sum(bandit.pull_count)
+        
+        # unpulled arms should always take priority
+        unpulled_arms = np.where(bandit.pull_count == 0)[0]
+        
+        # this check is important because if an arm is unpulled then the pull count is 0 and this will result in a division of 0
+        if len(unpulled_arms) > 0:
+            # if any arms are unpulled return the first unpulled arms index
+            return unpulled_arms[0]
+        
+        ucb_values = bandit.estimated_values + self.c * np.sqrt(np.log(total_pulls) / bandit.pull_count)
+        
+        # return the index of the arm which results in the highest ucb value
+        return np.argmax(ucb_values)
